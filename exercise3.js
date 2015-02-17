8e7d93336582e09f427d244716c927ea8c2a05d2
Samuel Fortier-Galarneau <sgalarneau@digium.com>
2015-02-17 14:53:42 -0600

    Exercise 3 solution.

---
+++ exercise3.js
@@ -0,0 +1,14 @@
+var Hapi = require('hapi');
+var Path = require('path');
+var server = new Hapi.Server();
+
+server.connection({
+  host: 'localhost',
+  port: Number(process.argv[2] || 8080)
+});
+
+server.route({path: '/', method:'GET', handler: {
+  file: Path.join(__dirname, '/index.html')
+}});
+
+server.start();
