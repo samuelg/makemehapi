1b6ebebb222b9d55d505631682bffc6641e3ce3e
Samuel Fortier-Galarneau <sgalarneau@digium.com>
2015-02-17 17:16:20 -0600

    Exercise 5 solution.

---
+++ exercise5.js
@@ -0,0 +1,21 @@
+var Hapi = require('hapi');
+var Path = require('path');
+var server = new Hapi.Server();
+
+server.connection({
+  host: 'localhost',
+  port: Number(process.argv[2] || 8080)
+});
+
+server.views({
+  engines: {
+    html: require('handlebars')
+  },
+  path: Path.join(__dirname, 'templates')
+});
+
+server.route({path: '/', method:'GET', handler: {
+  view: 'index.html'
+}});
+
+server.start();
